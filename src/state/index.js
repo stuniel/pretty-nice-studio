123b2a51311618ef44c96e167d2f9b7de4da00de
Daniel Studziński <stuniel@gmail.com>
2019-01-24 18:15:07 +0100

    Fix createStore arguments

--- src/state/index.js
+++ src/state/index.js
@@ -7,12 +7,14 @@ import path from './path/reducers'
 const initialState = { slide: 0, path: '/' }
 
 export const store = createStore(
-  combineReducers({
-    slide,
-    path,
-  }),
-  initialState,
-  typeof window !== 'undefined' &&
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()
+  combineReducers(
+    {
+      slide,
+      path,
+    },
+    initialState,
+    typeof window !== 'undefined' &&
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 )
